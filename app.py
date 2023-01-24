42fc90e93d4916389fdbfdbfe71d18a0de303c68
Jerwin Peralta <jerwin_tuchi21@yahoo.com>
2023-01-24 12:59:15 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -248,14 +248,14 @@ threshold = 0.05
 
 # Add support levels to the figure
 for i in range(len(support)):
-    support_index = display_data.loc[display_data['Low']==support[i]].index[0]
-    if support_index + display_data.loc[display_data['Low']==support[i]].shape[0] < display_data.shape[0]:
-        x1 = display_data['Date'][support_index + display_data.loc[display_data['Low']==support[i]].shape[0]]
+    index = display_data.loc[display_data['Low']==support[i]].index[0]
+    if index + display_data.loc[display_data['Low']==support[i]].shape[0] < len(display_data['Date']):
+        x1 = display_data['Date'][index + display_data.loc[display_data['Low']==support[i]].shape[0]]
     else:
         x1 = display_data['Date'].iloc[-1]
     fig3.add_shape(
         type='line',
-        x0=display_data['Date'][support_index],
+        x0=display_data['Date'][index],
         y0=support[i],
         x1=x1,
         y1=support[i],
@@ -264,14 +264,14 @@ for i in range(len(support)):
 
 # Add resistance levels to the figure
 for i in range(len(resistance)):
-    resistance_index = display_data.loc[display_data['High']==resistance[i]].index[0]
-    if resistance_index + display_data.loc[display_data['High']==resistance[i]].shape[0] < display_data.shape[0]:
-        x1 = display_data['Date'][resistance_index + display_data.loc[display_data['High']==resistance[i]].shape[0]]
+    index = display_data.loc[display_data['High']==resistance[i]].index[0]
+    if index + display_data.loc[display_data['High']==resistance[i]].shape[0] < len(display_data['Date']):
+        x1 = display_data['Date'][index + display_data.loc[display_data['High']==resistance[i]].shape[0]]
     else:
         x1 = display_data['Date'].iloc[-1]
     fig3.add_shape(
         type='line',
-        x0=display_data['Date'][resistance_index],
+        x0=display_data['Date'][index],
         y0=resistance[i],
         x1=x1,
         y1=resistance[i],
