792e8d9b185cb1b498a7636af9402eac75b1581f
Jerwin Peralta <jerwin_tuchi21@yahoo.com>
2023-01-24 13:56:23 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -290,6 +290,8 @@ fig3.update_xaxes(griddash='dash', gridwidth=1, gridcolor='#535566')
 fig3.update_yaxes(griddash='dash', gridwidth=1, gridcolor='#535566')
 fig3.layout.update(title="Support and Resistance Levels",title_font_size=35,title_x=0.5)
 fig3.update_layout(height=1000)
+
+fig3.show(config={'modeBarButtonsToAdd': ['drawline', 'drawopenpath', 'drawclosedpath', 'drawcircle', 'drawrect', 'eraseshape']})
 st.plotly_chart(fig3, True)
 
 # fig2.add_trace(go.Scatter(x=display_data_w.Date, y=display_data_w.Close, name="Price"))
