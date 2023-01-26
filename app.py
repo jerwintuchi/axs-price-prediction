f9b38df38cc43b4792b9d8f9a7465a588ac09946
Jerwin Peralta <jerwin_tuchi21@yahoo.com>
2023-01-26 11:32:47 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -199,7 +199,7 @@ fig.add_trace(go.Scatter(x=display_data.Date,
                         line=dict(color='red', width=2), 
                         name='5d MA'))
 #st.write(display_data.isna().sum())
-st.write(display_data.type())
+st.write(type(display_data))
                         
 fig2 = go.Figure() #RSI Chart
 # Create an empty list to store the color values
