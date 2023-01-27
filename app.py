d5cdaede0133a9f1f16b888358e56d101089849a
Jerwin Peralta <jerwin_tuchi21@yahoo.com>
2023-01-27 10:43:43 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -296,7 +296,9 @@ fig3.update_layout(height=1500)
 
 # Create a threshold variable to set the minimum distance between lines
 threshold = 0.05
-st.checkbox("Moving Average")
+if st.checkbox('Moving Average'):
+    fig3.add_trace(go.Scatter(x=display_data['Date'], y=display_data["ma"], line=dict(color='blue', width=1.5)))
+    fig3.add_trace(go.Scatter(x=display_data['Date'], y=display_data["ma5"], line=dict(color='purple', width=1.5)))
 
 if st.checkbox('Support and Resistance'): # SHOW SUPPORT AND RESISTANCE BUTTON
 # Add support levels to the figure
@@ -368,19 +370,6 @@ if st.checkbox('Support and Resistance'): # SHOW SUPPORT AND RESISTANCE BUTTON
     )
     st.plotly_chart(fig3, True)
 
-if st.checkbox("Moving Average"):
-    st.set_checkbox_value("Moving Average", True)
-    fig3.add_trace(go.Scatter(x=display_data.Date, 
-                        y=display_data['ma'], 
-                        opacity=0.7, 
-                        line=dict(color='orange', width=3), 
-                        name='13d MA'))
-
-    fig3.add_trace(go.Scatter(x=display_data.Date, 
-                        y=display_data['ma5'], 
-                        opacity=0.7, 
-                        line=dict(color='red', width=2), 
-                        name='5d MA'))
 
 else:
 # Only show the candlestick chart if the "Support and Resistance Chart" checkbox is not ticked
