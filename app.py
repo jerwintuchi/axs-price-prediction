2d20de385f2fe362768c7499809bcde0e12967fa
Jerwin Peralta <jerwin_tuchi21@yahoo.com>
2023-01-27 11:48:44 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -296,8 +296,8 @@ fig3.update_layout(height=1500)
 
 # Create a threshold variable to set the minimum distance between lines
 threshold = 0.05
+st.checkbox('Moving Average')
     
-
 if st.checkbox('Support and Resistance'): # SHOW SUPPORT AND RESISTANCE BUTTON
 # Add support levels to the figure
     for i in range(len(support)):
@@ -362,9 +362,11 @@ if st.checkbox('Support and Resistance'): # SHOW SUPPORT AND RESISTANCE BUTTON
     )
     st.plotly_chart(fig3, True)
 
-if st.checkbox('Moving Average'):
+if st.checkbox("Moving Average"):
     fig3.add_trace(go.Scatter(x=display_data['Date'], y=display_data["ma"], line=dict(color='blue', width=1.5), name="13 Candle MA"))
-    fig3.add_trace(go.Scatter(x=display_data['Date'], y=display_data["ma5"], line=dict(color='yellow', width=1.5), name="5 Candle MA"))   
+    fig3.add_trace(go.Scatter(x=display_data['Date'], y=display_data["ma5"], line=dict(color='yellow', width=1.5), name="5 Candle MA"))
+
+    st.plotly_chart(fig3, True)
 
 
 else:
