a08eb9fef125679612ae6c5738d2b01f72e0fb4c
Jerwin Peralta <jerwin_tuchi21@yahoo.com>
2023-01-27 10:19:22 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -368,7 +368,7 @@ if st.checkbox('Support and Resistance'): # SHOW SUPPORT AND RESISTANCE BUTTON
     )
     st.plotly_chart(fig3, True)
 
-if st.checkbox("Moving Average"):
+if st.sidebar.checkbox("Moving Average"):
     fig3.add_trace(go.Scatter(x=display_data.Date, 
                         y=display_data['ma'], 
                         opacity=0.7, 
