c159f0e56041c82c0160d1e52be52841cfaeaee9
Jerwin Peralta <jerwin_tuchi21@yahoo.com>
2023-01-27 10:53:51 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -294,10 +294,6 @@ fig3.update_yaxes(griddash='dash', gridwidth=1, gridcolor='#535566')
 fig3.update_layout(height=1500)
 
 
-# CHECKBOXES
-moving_average_checkbox = st.checkbox('Moving Average')
-support_resistance_checkbox = st.checkbox('Support and Resistance')
-
 # Create a threshold variable to set the minimum distance between lines
 threshold = 0.05
 if st.checkbox('Moving Average'):
@@ -305,41 +301,40 @@ if st.checkbox('Moving Average'):
     fig3.add_trace(go.Scatter(x=display_data['Date'], y=display_data["ma5"], line=dict(color='purple', width=1.5)))
 
 if st.checkbox('Support and Resistance'): # SHOW SUPPORT AND RESISTANCE BUTTON
-    if moving_average_checkbox.isChecked() and not support_resistance_checkbox.isChecked():
 # Add support levels to the figure
-        for i in range(len(support)):
-            index = display_data.loc[display_data['Low']==support[i]].index[0]
-            try:
-                next_support = support[i+1]
-                next_support_index = display_data.loc[display_data['Low']==next_support].index[0]
-                x1 = display_data['Date'][next_support_index]
-            except:
-                x1 = display_data['Date'].iloc[-1]
-            fig3.add_shape(
-                type='line',
-                x0=display_data['Date'][index],
-                y0=support[i],
-                x1=x1,
-                y1=support[i],
-            line=dict(color='green', width=3, dash='dot')
-            )
+    for i in range(len(support)):
+        index = display_data.loc[display_data['Low']==support[i]].index[0]
+        try:
+            next_support = support[i+1]
+            next_support_index = display_data.loc[display_data['Low']==next_support].index[0]
+            x1 = display_data['Date'][next_support_index]
+        except:
+            x1 = display_data['Date'].iloc[-1]
+        fig3.add_shape(
+            type='line',
+            x0=display_data['Date'][index],
+            y0=support[i],
+            x1=x1,
+            y1=support[i],
+         line=dict(color='green', width=3, dash='dot')
+        )
 # Add resistance levels to the figure
-        for i in range(len(resistance)):
-            indexr = display_data.loc[display_data['High']==resistance[i]].index[0]
-            try:
-                next_resistance = resistance[i+1]
-                next_resistance_index = display_data.loc[display_data['High']==next_resistance].index[0]
-                x1 = display_data['Date'][next_resistance_index]
-            except:
-                x1 = display_data['Date'].iloc[-1]
-            fig3.add_shape(
-                type='line',
-                x0=display_data['Date'][indexr],
-                y0=resistance[i],
-                x1=x1,
-                y1=resistance[i],
-                line=dict(color='red', width=3, dash='dot')
-            )
+    for i in range(len(resistance)):
+        indexr = display_data.loc[display_data['High']==resistance[i]].index[0]
+        try:
+         next_resistance = resistance[i+1]
+         next_resistance_index = display_data.loc[display_data['High']==next_resistance].index[0]
+         x1 = display_data['Date'][next_resistance_index]
+        except:
+            x1 = display_data['Date'].iloc[-1]
+        fig3.add_shape(
+            type='line',
+            x0=display_data['Date'][indexr],
+            y0=resistance[i],
+            x1=x1,
+            y1=resistance[i],
+            line=dict(color='red', width=3, dash='dot')
+        )
 
 # Removing duplicates values
     support = list(set(support))
