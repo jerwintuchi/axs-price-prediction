ac38057262a203595272f974ebabe2b5748d8bd2
Jerwin Peralta <jerwin_tuchi21@yahoo.com>
2023-02-11 11:52:49 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -301,13 +301,13 @@ options = st.multiselect(
     'Select your Technical Indicator(s)',
     ['Moving Average', 'Support & Resistance'])
 
-if options == options[0]:
+if options == options['Moving Average']:
     fig3.add_trace(go.Scatter(x=display_data['Date'], y=display_data["ma"], line=dict(color='blue', width=1.5), name="13 Candle MA"))
     fig3.add_trace(go.Scatter(x=display_data['Date'], y=display_data["ma5"], line=dict(color='yellow', width=1.5), name="5 Candle MA"))
 
 
 
-if options == options[1]: # SHOW SUPPORT AND RESISTANCE BUTTON
+if options == options['Support & Resistance']: # SHOW SUPPORT AND RESISTANCE BUTTON
 # Add support levels to the figure
     for i in range(len(support)):
         index = display_data.loc[display_data['Low']==support[i]].index[0]
